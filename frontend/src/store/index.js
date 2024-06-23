8c22dc37266e552e208caf6b94903858e4cd6166
TheSaviourEking <saviour.eking@outlook.com>
2024-06-24 00:55:46 +0100

    chore: update my reducers and configureStore functions

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,20 +1,25 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import { thunk } from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+
+import sessionReducer from "./session";
 
 const rootReducer = combineReducers({
-
-})
+    session: sessionReducer,
+});
 
 let enhancer = null;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
     enhancer = applyMiddleware(thunk);
 } else {
-    const logger = require('redux-logger').default;
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const logger = require("redux-logger").default;
+    const composeEnhancers =
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     enhancer = composeEnhancers(applyMiddleware(thunk, logger));
 }
 
-const configureStore = (proloadedState) => (createStore(rootReducer, proloadedState, enhancer));
+const configureStore = (preloadedState) => {
+    return createStore(rootReducer, preloadedState, enhancer);
+};
 
 export default configureStore;
